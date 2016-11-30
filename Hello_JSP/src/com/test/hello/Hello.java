146fc4809bddc2028318e3df1a64f44e7654ed60
KJH <mint_star_@naver.com>
2016-11-30 11:50:22 +0900

    modify>>Hello.java:012456 by KJH

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -5,7 +5,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("local Rpository create Hello_jsp");
-		
+		// 012456:hotfix edit by KJH
+		System.out.println("hotfix edit/modify");
+		// 012456:end
 	}
 
 }
