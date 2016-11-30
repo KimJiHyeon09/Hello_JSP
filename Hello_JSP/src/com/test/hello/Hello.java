0d2f62883fdcee83a54013e24718fec38efbda6c
KimJiHyeon09 <mint_star_@naver.com>
2016-11-30 12:31:52 +0900

    modify >> Hello.java:0215238 by KJH

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -8,6 +8,9 @@ public class Hello {
 		// 012456:hotfix edit by KJH
 		System.out.println("hotfix edit/modify");
 		// 012456:end
+		
+		// 0215238:origin/master edit by KJH
+		System.out.println("GitGHub edit");
 	}
 
 }
